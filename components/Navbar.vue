f3c9f5812a38f1e64b74db7b487291ca0f74df1d
unknown <crsthian.04@gmail.com>
2024-01-26 16:24:56 -0300

    background hero

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-100 flex items-center bg-[#88888878] h-16 rounded-b-[26px]">
+    <div class="w-dvw mx-20 flex items-center justify-between">
+      <img
+        src="../assets/images/logo.png"
+        alt=""
+        class="w-44 object-contain cursor-pointer"
+      />
+
+      <div class="text-[28px] space-x-24 text-white outline-brown">
+        <Nuxt-Link to="/artists">Artists</Nuxt-Link>
+        <Nuxt-Link to="/brands">Brands</Nuxt-Link>
+        <Nuxt-Link to="/models">Models</Nuxt-Link>
+        <Nuxt-Link to="/about">About</Nuxt-Link>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="w-100 flex items-center bg-slate-500 h-16 rounded-b-[26px]">
-    <div class="w-dvw mx-20 flex items-center justify-between">
-      <img src="../assets/images/logo.png" alt="" class="w-44 object-contain" />
-
-      <div class="text-[28px] space-x-24 text-white outline-brown">
-        <Nuxt-Link to="/">Artists</Nuxt-Link>
-        <Nuxt-Link to="/">Brands</Nuxt-Link>
-        <Nuxt-Link to="/">Models</Nuxt-Link>
-        <Nuxt-Link to="/">About</Nuxt-Link>
-      </div>
-    </div>
+  <div
+    class="bg-[url(~/assets/images/hero-image.png)] w-100 h-screen bg-cover bg-left"
+  >
+    <Navbar></Navbar>
   </div>
 </template>
