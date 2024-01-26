5961c31b3d841fa51a6bfffee11a0604266cb349
unknown <crsthian.04@gmail.com>
2024-01-26 15:48:19 -0300

    navbar

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-100 flex items-center bg-slate-500 h-16 rounded-b-[26px]">
+    <div class="w-dvw mx-20 flex items-center justify-between">
+      <img src="../assets/images/logo.png" alt="" class="w-44 object-contain" />
+
+      <div class="text-[28px] space-x-24 text-white outline-brown">
+        <Nuxt-Link to="/">Artists</Nuxt-Link>
+        <Nuxt-Link to="/">Brands</Nuxt-Link>
+        <Nuxt-Link to="/">Models</Nuxt-Link>
+        <Nuxt-Link to="/">About</Nuxt-Link>
+      </div>
+    </div>
+  </div>
+</template>
