d5acbb27991452e724a736f0abe9489cfbe2a6a7
unknown <crsthian.04@gmail.com>
2024-02-02 15:51:06 -0300

    improve database connection

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,6 @@
-import { MongoClient } from "mongodb";
+import db from "../database/connection";
 
 export default defineEventHandler(async () => {
-  const client = new MongoClient(process.env.STRING_CONNECTION || "");
-  const db = client.db("WoodStock");
-  const artists = db.collection("artists");
-  return artists.find();
+  let guitars = db.collection("guitars");
+  return guitars.find({}).toArray();
 });
-
-//mongodb+srv://cristhian:cristhian123@cluster1.qbxal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-//mongodb+srv://crsthian04:cristhian123@woodstock.v29v5pl.mongodb.net/
--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,11 +1,4 @@
-import { connect } from "mongoose";
-
-export async function run() {
-  try {
-    process.env.STRING_CONNECTION
-      ? await connect(process.env.STRING_CONNECTION)
-      : console.log("Erro de conecção");
-  } catch {
-    console.log("Error de conecção");
-  }
-}
+import { MongoClient } from "mongodb";
+const client = new MongoClient(process.env.STRING_CONNECTION || "");
+client.connect();
+export default client.db("WoodStock");
