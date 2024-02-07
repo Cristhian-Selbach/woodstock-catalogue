2599700e72dbea3773502e2abb941f73a67e78ec
unknown <crsthian.04@gmail.com>
2024-02-07 19:22:31 -0300

    add route get brands

---
+++ server/api/brands.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import Brands from "../database/models/Brands";
+
+export default defineEventHandler(async (event) => {
+  try {
+    mongoose.connect(
+      process.env.STRING_CONNECTION || "mongodb://localhost:27017"
+    );
+
+    return Brands.find({});
+  } catch (error) {
+    console.log("Error " + error);
+    return "err";
+  }
+});
