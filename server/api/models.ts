c0e789e073fef87683368f328422b2a972389d61
unknown <crsthian.04@gmail.com>
2024-02-09 11:40:37 -0300

    add models route

---
+++ server/api/models.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import Models from "../database/models/Models";
+
+export default defineEventHandler(async (event) => {
+  try {
+    mongoose.connect(
+      process.env.STRING_CONNECTION || "mongodb://localhost:27017"
+    );
+
+    return Models.find({});
+  } catch (error) {
+    console.log("Error " + error);
+    return "err";
+  }
+});
