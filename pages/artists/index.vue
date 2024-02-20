693f9b9cca87068eadb0d91c08ac9f20592dd382
unknown <crsthian.04@gmail.com>
2024-02-20 16:08:36 -0300

    artists page responsive

--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -4,13 +4,21 @@ const artists = data.value;
 </script>
 
 <template>
+  <!-- BACKGROUND MOBILE (because background-fixed is not supported on safari ) -->
+  <div
+    class="bg-[url(~/assets/images/artists-background.png)] w-[100vw] h-[100vh] fixed bg-cover bg-center sm:hidden"
+  ></div>
+
   <div
     class="bg-[url(~/assets/images/artists-background.png)] bg-cover bg-center justify-between bg-fixed"
   >
     <Navbar></Navbar>
-    <div class="mx-20 grid grid-cols-3 gap-[10vw] mt-32">
+
+    <div
+      class="mx-[10vw] sm:mx-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-16 sm:gap-[10vw] mt-12 md:mt-32 relative"
+    >
       <Nuxt-Link
-        :to="`artists/${artist.slug}`"
+        :to="`/${artist.slug}`"
         v-for="artist in artists"
         class="h-[450px] bg-cover bg-center text-white flex justify-center items-center rounded-[45px] outline-cards hover-effect-cards"
         :style="`background-image: url(${artist.images.displayImgUrl})`"
