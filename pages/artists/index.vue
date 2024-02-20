0e3c9c87beac601cdd6e2377948b75c8bf6433fb
unknown <crsthian.04@gmail.com>
2024-02-20 14:46:31 -0300

    details on artists page

--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -5,19 +5,20 @@ const artists = data.value;
 
 <template>
   <div
-    class="bg-[url(~/assets/images/artists-background.png)] w-100 h-screen bg-cover bg-center justify-between bg-fixed"
+    class="bg-[url(~/assets/images/artists-background.png)] bg-cover bg-center justify-between bg-fixed"
   >
     <Navbar></Navbar>
     <div class="mx-20 grid grid-cols-3 gap-[10vw] mt-32">
-      <div
+      <Nuxt-Link
+        :to="`artists/${artist.slug}`"
         v-for="artist in artists"
-        class="h-[450px] bg-cover bg-center text-white flex justify-center items-center rounded-[45px]"
+        class="h-[450px] bg-cover bg-center text-white flex justify-center items-center rounded-[45px] outline-cards hover-effect-cards"
         :style="`background-image: url(${artist.images.displayImgUrl})`"
       >
-        <h1 class="text-[110px] outline-title-gray">
+        <h1 class="text-[100px] outline-title-gray">
           {{ artist.displayName }}
         </h1>
-      </div>
+      </Nuxt-Link>
     </div>
   </div>
 </template>
