c14615d04e6e337f43a931d79fa4d14f15003e46
unknown <crsthian.04@gmail.com>
2024-08-24 10:29:58 -0300

    update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,12 +25,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
-        },
-      ],
+      script: [{ src: "https://kit.fontawesome.com/4a53af05d6.js" }],
     },
   },
 });
