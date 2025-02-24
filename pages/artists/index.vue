7ecb50df21436fb29ecbc22edaf91896bcced41a
Cristhian Selbach <crsthian.04@gmail.com>
2025-02-24 15:45:51 -0300

    fixed bugs on nuxt-link

--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -17,8 +17,8 @@ const artists = data.value;
     <div
       class="mx-[10vw] xl:mx-52 sm:mx-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-16 sm:gap-[10vw] xl:gap-[5vw] mt-12 md:mt-32 relative"
     >
-      <Nuxt-Link
-        :to="`artists/${artist.slug}`"
+      <a
+        :href="`artists/${artist.slug}`"
         v-for="artist in artists"
         class="h-[450px] bg-cover bg-center text-white flex justify-center items-center rounded-[45px] outline-cards hover-effect-cards"
         :style="`background-image: url(${artist.images.displayImgUrl})`"
@@ -26,7 +26,7 @@ const artists = data.value;
         <h1 class="text-[100px] outline-title-gray">
           {{ artist.displayName }}
         </h1>
-      </Nuxt-Link>
+      </a>
     </div>
   </div>
 </template>
